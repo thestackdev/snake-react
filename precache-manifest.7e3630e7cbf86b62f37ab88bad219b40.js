7bb012271d9155c1045a641dc8ecb056e42cb7e6
shanmukeshwar09 <shanmukeshwar09@gmail.com>
2020-09-02 11:58:56 +0530

    Updates

---
+++ precache-manifest.7e3630e7cbf86b62f37ab88bad219b40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b843840caf2051d4d2a764c86746703",
+    "url": "/snake-react/index.html"
+  },
+  {
+    "revision": "1cedd5dbc3e9b4ef694b",
+    "url": "/snake-react/static/css/main.63099224.chunk.css"
+  },
+  {
+    "revision": "e2b78c78ab09c1ba1143",
+    "url": "/snake-react/static/js/2.09a6f9bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snake-react/static/js/2.09a6f9bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cedd5dbc3e9b4ef694b",
+    "url": "/snake-react/static/js/main.e3631ed9.chunk.js"
+  },
+  {
+    "revision": "ddf9624cea2d350925ba",
+    "url": "/snake-react/static/js/runtime-main.9d2312e0.js"
+  }
+]);
